1afe888370b89590af9ab24d422b52bc0b3b26cf
CiroTello <ciroleandromzzza@gmail.com>
2023-10-04 17:36:32 -0300

    s

--- src/main/java/com/Service/EnvioService.java
+++ src/main/java/com/Service/EnvioService.java
@@ -4,7 +4,7 @@ import com.Domain.Envio;
 import org.springframework.stereotype.Service;
 
 
-//ooooooooooo
+//solo quiero que se ejecute cuando se llame a la clase
 @Service
 public class EnvioService {
     public Envio crearEnvio(Envio envio) {
