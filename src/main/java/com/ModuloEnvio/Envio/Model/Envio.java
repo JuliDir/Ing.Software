24ebdcfb067c8fc0cc5c51930f14e337d366494d
unknown <jgdirocco@gmail.com>
2023-10-03 21:49:38 -0300

    prueba

--- src/main/java/com/ModuloEnvio/Envio/Model/Envio.java
+++ src/main/java/com/ModuloEnvio/Envio/Model/Envio.java
@@ -12,7 +12,7 @@ public class Envio {
     private EstadoEnvio estadoEnvio;
     private Producto producto;
     private double costoEnvio;
-    private tring observacionesEnvio;
+    private ring observacionesEnvio;
 
     public Envio(){};
 
