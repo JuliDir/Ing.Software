7a24c3123417fcc48034d901d18807b4c6b5c80d
Ciro TELLO <ciro.tello@dgi.loc>
2023-10-05 09:17:19 -0300

    o

--- src/main/java/com/EnvioApplication.java
+++ src/main/java/com/EnvioApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class EnvioApplication {
-	//taggzzz
+	//taggssdsdsdsdsds
 	public static void main(String[] args) {
 		SpringApplication.run(EnvioApplication.class, args);
 	}
