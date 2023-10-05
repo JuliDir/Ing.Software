9353e08147432669b43753f5557a9a9abc98ca09
JuliDir <100112014+JuliDir@users.noreply.github.com>
2023-10-05 11:52:24 -0300

    Update CorsConfig.java

--- src/main/java/com/CorsConfig.java
+++ src/main/java/com/CorsConfig.java
@@ -1,13 +1,14 @@
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class CorsConfig extends WebMvcConfigurerAdapter {
+public class CorsConfig implements WebMvcConfigurer {
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")  // Map the path you want to enable CORS for
-                .allowedOrigins("*")  // Allowed origins
+        registry.addMapping("/api/*")  // Map the path you want to enable CORS for
+                .allowedOrigins("")  // Allow requests from all origins
                 .allowedMethods("GET", "POST", "PUT", "DELETE")  // Allowed methods
                 .allowedHeaders("Content-Type", "Authorization");  // Allowed headers
     }
